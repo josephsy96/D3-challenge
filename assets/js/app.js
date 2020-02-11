2f2a4b7542c2c215a84279f8b1ccade27bfbb88c
josephsy96 <josephsy96@hotmail.com>
2020-02-10 19:03:25 -0600

    minor tweaks

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,12 @@
-// @TODO: YOUR CODE HERE!
+//set chart height and width
+let height_svg = 1080;
+let width_svg = 1920;
+
+//Load csv file
+
+d3.csv("data/data.csv").then(function(news_data) {
+    console.log(news_data.state);
+
+}).catch(function(error) {
+    console.log(error);
+});
