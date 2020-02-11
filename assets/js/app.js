e1b57c7c942ba42f6298e6872252f95c3b44c359
josephsy96 <josephsy96@hotmail.com>
2020-02-10 21:21:35 -0600

    add new web style

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,41 @@
 //set chart height and width
-let height_svg = 1080;
-let width_svg = 1920;
+let height_svg = 900;
+let width_svg = 1000;
 
+//Set default margins
+let margin = {
+    top: 30,
+    right: 50,
+    left: 50,
+    bottom: 80
+};
+
+
+let width = height_svg - margin.left - margin.right;
+let height = width_svg - margin.top - margin.bottom;
+
+//Create the wrapper for the svg scatter plot
+let news_svg = d3.select("#scatter")
+            .append("svg")
+            .attr("width", width_svg)
+            .attr("height", height_svg);
+
+//Set the chart svg
+let scatter_chart = news_svg.append("g")
+                .attr("transform", `translate(${margin.left}, ${margin.right})`);
+
+//==========================================================
 //Load csv file
+d3.csv("assets/data/data.csv").then(function(news_data) {
+    console.log(news_data[0]);
 
-d3.csv("data/data.csv").then(function(news_data) {
-    console.log(news_data.state);
+    Object.entries(news_data).forEach(([key,news]) => {
+        //Test object load
+        console.log(news.state);
+    });
+
 
 }).catch(function(error) {
     console.log(error);
 });
+//==========================================================
