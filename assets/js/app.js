df5c39e732bd11660ccb5266a46f239b57ab441f
josephsy96 <josephsy96@hotmail.com>
2020-02-11 19:44:19 -0600

    chart properties

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,10 +19,76 @@ let news_svg = d3.select("#scatter")
             .append("svg")
             .attr("width", width_svg)
             .attr("height", height_svg);
+//==========================================================
 
 //Set the chart svg
 let scatter_chart = news_svg.append("g")
                 .attr("transform", `translate(${margin.left}, ${margin.right})`);
+
+//Set default x-axis data
+
+let x_news = "poverty";
+
+//==========================================================
+//Update x axis scale for scatter chart
+function X_Scale(health, x_value) {
+    //creates the scales
+    let x_linear_scale = d3.scaleLinear()
+                        .domain([d3.min(health, h => h[x_value]) * 0.8,
+                        d3.max(health, h => h[x_value]) * 1.2
+                    ]).range([0, width]);
+    return x_linear_scale;
+}
+
+//==========================================================
+//Update the x axis values
+function render_axes(new_x_scale, x_axis) {
+    let bottom_axis = d3.axisBottom(new_x_scale);
+
+    x_axis.transition()
+            .duration(1200)
+            .call(bottom_axis);
+
+    return x_axis;
+}
+
+//==========================================================
+//Render Chart Circles
+function render_dots(circle_group, newXScale, x_value) {
+    circle_group.transition()
+                .duration(1200)
+                .attr("cx", cir => newXScale(cir[x_value]));
+    return circle_group;
+}
+
+//==========================================================
+//function to update the circle groups
+function update_tips(x,cg) {
+    if (x === "poverty") {
+        let label = "In Poverty (%)";
+    }
+    else {
+        let label = "Age (Median)";
+    }
+
+    let chart_tip = d3.tip()
+                      .attr("class","chart-tip")
+                      .offset([80, 60])
+                      .html(function(t) {
+        return (`${t.state}<br>${label} ${t[x]}`);
+                      });
+
+    cg.call(chart_tip);
+
+    //mouseover data
+    cg.on("mouseover", function(n_data) {
+        chart_tip.show(n_data);
+    }).on("mouseout", function(n_data, index) { //event when mouse hovers over circle
+            chart_tip.hide(n_data);
+        });
+    
+    return cg;
+    }
 
 //==========================================================
 //Load csv file
@@ -31,7 +97,9 @@ d3.csv("assets/data/data.csv").then(function(news_data) {
 
     Object.entries(news_data).forEach(([key,news]) => {
         //Test object load
-        console.log(news.state);
+        // console.log(news.state);
+        news.poverty = +news.poverty;
+        news.healthcare = +news.healthcare;
     });
 
 
